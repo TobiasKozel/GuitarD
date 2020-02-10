83b68fd89d73e43d4efa35c00d0477a961afe033
Tobias Kozel <t.kozel@protonmail.com>
2020-02-10 04:33:46 +0100

    more work on soundwoofer

--- thirdparty/soundwoofer/soundwoofer.h
+++ thirdparty/soundwoofer/soundwoofer.h
@@ -4,6 +4,8 @@
   #include "json.hpp"
 #endif
 
+#define SOUNDWOOFER_NO_API
+
 #ifndef SOUNDWOOFER_NO_API
   #ifndef SOUNDWOOFER_CUSTOM_HTTP
     // #define CPPHTTPLIB_OPENSSL_SUPPORT
@@ -28,7 +30,11 @@
   #include <sys/stat.h>
 #endif
 
+#include <iostream>
+#include <sstream>
 #include <fstream>
+#include <mutex>
+#include <iomanip>
 
 class SoundWoofer {
 public:
@@ -41,7 +47,8 @@ public:
     PLUGIN_NAME_NOT_SET,
     WAV_ERROR,
     NOT_IMPLEMENTED,
-    JSON_ENCODE_ERROR
+    JSON_ENCODE_ERROR,
+    UNKNOWN_IR, // This happens when a SWImpulse is passed as an argument which is not in mIRList
   };
 
   /**
@@ -53,30 +60,50 @@ public:
    */
   struct SWImpulse {
     std::string id;
-    int micX;
-    int micY;
-    int micZ;
-    std::string micId;
     std::string name;
+    std::string micId;
     std::string rig;
-    std::string file;
+    std::string file; // This is either a uuid or a path
+    int micX = 0;
+    int micY = 0;
+    int micZ = 0;
     std::string description;
     int micPosition;
     std::string userName;
     std::string element;
+    bool local = false;
     int channels = 0;
     int length = 0;
     float** samples = nullptr;
   };
 
-  struct SWMic {
+  const std::string TypeMicrophone = "Microphone";
+  const std::string TypeCabinet = "Cabinet";
+
+  struct SWComponent {
     std::string id;
-    // TODO Mic object doesn't exist on the backend
+    std::string name;
+    std::string type; // "Microphone", "Cabinet"
+    int componentBase = 0;
+    std::string desciption; // TODO there's a typo in the backend
+    int year = 0;
+    std::string url;
+    std::string userName;
+    std::string brand;
+    std::string model;
+    std::string baseComponentDescription;
+    bool local = false;
+
   };
 
   struct SWRig {
     std::string id;
     std::string name;
+    std::string description;
+    std::string username;
+    std::vector<SWComponent> components;
+    std::vector<SWComponent> microphones;
+    std::vector<SWImpulse> impulses;
   };
 
   /**
@@ -87,8 +114,9 @@ public:
     std::string id;
     std::string plugin;
     std::string data;
-    std::string version;
-    bool local = true;
+    std::string version = "-1";
+    std::vector<std::string> tags;
+    bool local = false; // This means the preset is not from the server
   };
 
   /**
@@ -105,6 +133,7 @@ public:
 
   typedef std::vector<SWImpulse> SWImpulses;
   typedef std::vector<SWRig> SWRigs;
+  typedef std::vector<SWComponent> SWComponents;
   typedef std::vector<SWPreset> SWPresets;
 
   /**
@@ -114,18 +143,30 @@ public:
 
 
 private:
+  const SWComponent GenericComponent = { "Generic Component", "Generic Component", TypeCabinet };
+  const SWComponent GenericMicrophone = SWComponent{ "Generic Microphone", "Generic Microphone", TypeMicrophone };
+  const SWRig UncategorizedRig = SWRig {
+    "Uncategorized", "Uncategorized", "", "Various", { GenericMicrophone }, { GenericComponent }
+  };
+
   const std::string PATH_DELIMITER =
 #ifdef _WIN32
     "\\";
 #else
     "/";
 #endif
+
+#ifndef SOUNDWOOFER_NO_API
   // const std::string BACKEND_URL = "svenssj.tech";
   const std::string API_VERSION = "0.1";
   const std::string BACKEND_URL = "localhost";
   const int BACKEND_PORT = 5000;
-  bool mUseIrCache = true;
-  std::string mPluginName = ""; // Plugin name used to label and filter presets by
+#endif
+  bool mCacheIRs = false;
+  bool mCachePresets = false;
+
+  std::string mPluginName; // Plugin name used to label and filter presets by
+  std::string mPluginVersion;
   std::string mHomeDirectory; // Directory for this plugin
   std::string mIrDirectory; // Directory for user IRs
   std::string mIrCacheDirectory; // Directory for caching online IRs
@@ -133,7 +174,8 @@ private:
   std::string mPresetDirectory; // Directory for user IRs
 
   SWImpulses mIRlist;
-  SWRigs mCabList;
+  SWRigs mRigList;
+  SWComponents mComponentList;
   SWPresets mPresetList;
 
   /**
@@ -167,8 +209,17 @@ private:
   }
 public:
 
-  void setPluginName(const std::string &name) {
+  /**
+   * Set this first, it's needed for caching and for online presets
+   */
+  void setPluginName(const std::string& name) {
     mPluginName = name;
+    mCacheIRs = true;
+    mCachePresets = true;
+  }
+
+  void setPluginVersion(const std::string& version) {
+    mPluginVersion = version;
   }
 
   /**
@@ -184,8 +235,10 @@ public:
     mIrCacheDirectory = path + "ir_cache" + PATH_DELIMITER;
     bool ok = true;
     ok = createFolder(path.c_str()) != SUCCESS ? false : ok;
+#ifndef SOUNDWOOFER_NO_API
     ok = createFolder(mPresetCacheDirectory.c_str()) != SUCCESS ? false : ok;
     ok = createFolder(mIrCacheDirectory.c_str()) != SUCCESS ? false : ok;
+#endif
     ok = createFolder(mPresetDirectory.c_str()) != SUCCESS ? false : ok;
     ok = createFolder(mIrDirectory.c_str()) != SUCCESS ? false : ok;
     return ok ? SUCCESS : GENERIC_ERROR;
@@ -199,6 +252,7 @@ public:
     return mPresetDirectory;
   }
 
+#ifndef SOUNDWOOFER_NO_API
   std::string getCacheIrPath() const {
     return mIrCacheDirectory;
   }
@@ -206,16 +260,70 @@ public:
   std::string getCachePresetPath() const {
     return mPresetCacheDirectory;
   }
+#endif
 
   /**
-   * Gets a list of all the Impulse responses from the server
+   * Gets a list of all the Impulse responses from the server and the local folder
    * Will also call clearCachedIRs()
    */
-  Status fetchIRs() {
+  Status listIRs() {
+    flushIRBuffers();
+    mRigList.clear();
+    mComponentList.clear();
+    mComponentList.push_back(GenericComponent);
+    mComponentList.push_back(GenericMicrophone);
+    mIRlist.clear();
+    SWRig uncategorized = UncategorizedRig;
+
+    auto addToUncategorized = [&](FileInfo& info) {
+      if (!isWaveName(info.name)) { return; }
+      SWImpulse ir = {"CHECKSUM", info.name, GenericMicrophone.id, uncategorized.id, info.relative };
+      ir.local = true;
+      uncategorized.impulses.push_back(ir);
+    };
+
+    if (!mIrDirectory.empty()) { // Gather the user IRs in the IR directory
+      auto cabLevel = scanDir(mIrDirectory);
+      for (auto i : cabLevel) {
+        if (i.isFolder) {
+          SWRig rig = { i.name, i.name };
+          auto micLevel = scanDir(i);
+          for (auto j : micLevel) {
+            bool micExists = false;
+            for (auto& m : mComponentList) {
+              if (m.name == j.name) {
+                micExists = true;
+                break;
+              }
+            }
+            SWComponent mic = { j.name, j.name, TypeMicrophone };
+            mic.local = true;
+            rig.microphones.push_back(mic);
+            if (!micExists) { mComponentList.push_back(mic); }  // GLOBAL
+            if (j.isFolder) {
+              auto posLevel = scanDir(j);
+              for (auto k : posLevel) {
+                if (!k.isFolder && isWaveName(k.name)) {
+                  SWImpulse ir = {"CHECKSUM", k.name, mic.id, rig.id, k.relative};
+                  ir.local = true;
+                  mIRlist.push_back(ir); // GLOBAL
+                  rig.impulses.push_back(ir);
+                }
+              }
+            } else { addToUncategorized(j); } // At mic level
+          }
+          mRigList.push_back(rig); // GLOBAL
+        } else { addToUncategorized(i); } // At Cabinet level
+      }
+    }
+
+    mRigList.push_back(uncategorized); // GLOBAL
+
+#ifndef SOUNDWOOFER_NO_API
     std::string data = httpGet("/Impulse");
     if (data.empty()) { return SERVER_ERROR; }
-    flushIRs();
     mIRlist = parseIRs(data);
+#endif
     //data = httpGet("/Rig");
     //if (data.empty()) { return SERVER_ERROR; }
     //mCabList = parseRigs(data);
@@ -223,31 +331,45 @@ public:
     // TODO assign them to the cabs an mics
   }
 
-  Status fetchIRs(Callback callback) {
+  Status listIRs(const Callback callback) {
     startAsync([&]() {
-      return this->fetchIRs();
+      return this->listIRs();
     }, callback);
     return ASYNC;
   }
 
   /**
-   * Fetches presets from the server
+   * Fetches presets from the server and the local folder
    */
-  Status fetchPresets() {
+  Status listPresets() {
+    mPresetList.clear();
+    if (!mPresetDirectory.empty()) {
+      auto files = scanDir(mPresetDirectory);
+      for (auto i : files) {
+        if (!i.isFolder && isJSONName(i.name)) {
+          SWPreset preset = { i.name, i.name, mPluginName };
+          preset.local = true;
+          mPresetList.push_back(preset);
+        }
+      }
+    }
+#ifndef SOUNDWOOFER_NO_API
     if (mPluginName.empty()) { return PLUGIN_NAME_NOT_SET; }
     std::string data = httpGet("/Preset");
     if (data.empty()) { return SERVER_ERROR; }
     mPresetList = parsePresets(data);
+#endif
     return SUCCESS;
   }
 
-  Status fetchPresets(Callback callback) {
+  Status fetchPresets(const Callback callback) {
     startAsync([&]() {
-      return this->fetchPresets();
+      return this->listPresets();
     }, callback);
     return ASYNC;
   }
 
+#ifndef SOUNDWOOFER_NO_API
   /**
    * Assembles a SWPreset and sends it to the server
    * Doesn't update the cached presets
@@ -281,6 +403,9 @@ public:
     return ASYNC;
   }
 
+  /**
+   * Load an IR from an id (Only works for Cloud IRs)
+   */
   Status loadIR(std::string id) {
     for (auto &i : mIRlist) {
       if (id == i.id) {
@@ -289,6 +414,7 @@ public:
     }
     return GENERIC_ERROR;
   }
+#endif
 
   /**
    * Download and decode a specific IR
@@ -297,21 +423,29 @@ public:
    * Will block, use a lambda as a callback for async
    */
   Status loadIR(SWImpulse& ir) {
-    if (ir.samples != nullptr) { return SUCCESS; } // already in ram
-    Status load = GENERIC_ERROR;
-    if (mUseIrCache) { // Hit the cache first
-      load = loadWave(ir);
-      if (load == SUCCESS) { return SUCCESS; }
+    SWImpulse* internalIr = nullptr;
+    for (auto& i : mIRlist) {
+      if (i.file == ir.file) { internalIr = &i; }
     }
+    if (internalIr == nullptr) { return UNKNOWN_IR; }
+    if (internalIr->samples != nullptr) { return SUCCESS; } // already in ram
+
+    Status load = GENERIC_ERROR;
+    load = loadWave(ir);
+    if (load == SUCCESS) { return SUCCESS; }
+
+#ifndef SOUNDWOOFER_NO_API
     const std::string result = httpGet("/File/Download/" + ir.file); // fetch it from the server
     if (result.empty()) { return SERVER_ERROR; }
     load = loadWave(ir, result.c_str(), result.size());
     if (load == SUCCESS && mUseIrCache) {
       writeFile((mIrCacheDirectory + ir.id).c_str(), result.c_str(), result.size());
     }
+#endif
     return load;
   }
 
+#ifndef SOUNDWOOFER_NO_API
   Status loadIR(std::string id, Callback callback) {
     for (auto& i : mIRlist) {
       if (id == i.id) {
@@ -320,6 +454,7 @@ public:
     }
     return GENERIC_ERROR;
   }
+#endif
 
   /**
    * Async version of downloadIR
@@ -331,6 +466,7 @@ public:
     return ASYNC;
   }
 
+#ifndef SOUNDWOOFER_NO_API
   SWImpulse* getIR(std::string id) {
     for (auto& i : mIRlist) {
       if (id == i.id) {
@@ -339,6 +475,7 @@ public:
     }
     return nullptr;
   }
+#endif
 
   SWImpulses& getIRs() {
     return mIRlist;
@@ -349,9 +486,9 @@ public:
   }
 
   /**
-   * Will clear out all IRs currently in ram
+   * Will clear out all IR buffers currently in ram
    */
-  void flushIRs() {
+  void flushIRBuffers() {
     for (auto& i : mIRlist) {
       if (i.samples != nullptr) {
         for (int c = 0; c < i.channels; c++) {
@@ -363,12 +500,14 @@ public:
     }
   }
 
+#ifndef SOUNDWOOFER_NO_API
   /**
    * Will clear out the disk cached IRs
    */
   void clearIRCache() {
     // TODO
   }
+#endif
 
   /**
    * Clears the async queue, but doesn't terminate a running task
@@ -424,6 +563,47 @@ public:
   }
 
   /**
+   * Creates a directory
+   * Returns success if the folder was created or already existing
+   */
+  static Status createFolder(const char* path) {
+#ifdef _WIN32
+    const bool ok = CreateDirectory(path, nullptr);
+    if (!ok) {
+      if (GetLastError() != ERROR_ALREADY_EXISTS) {
+        return GENERIC_ERROR; // Probably permission, locked file etc
+      }
+    }
+    return SUCCESS;
+#else 
+    mode_t process_mask = umask(0);
+    int result_code = mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO);
+    umask(process_mask);
+    return !result_code ? SUCCESS : GENERIC_ERROR;
+#endif
+  }
+
+  std::string hashFile(const char* path) {
+    std::ifstream fp(path);
+    std::stringstream ss;
+
+    // Unable to hash file, return an empty hash.
+    if (!fp.is_open()) {
+      return "";
+    }
+
+    // Hashing
+    uint32_t magic = 5381;
+    char c;
+    while (fp.get(c)) {
+      magic = ((magic << 5) + magic) + c; // magic * 33 + c
+    }
+
+    ss << std::hex << std::setw(8) << std::setfill('0') << magic;
+    return ss.str();
+  }
+
+  /**
    * Singleton stuff
    */
   SoundWoofer(const SoundWoofer&) {};
@@ -435,7 +615,7 @@ public:
   }
 
   virtual ~SoundWoofer() {
-    flushIRs();
+    flushIRBuffers();
     if (mThread.joinable()) {
       mThread.join();
     }
@@ -579,6 +759,7 @@ private:
    * Will do a synchronous http request to the url + port + the endpoint parameter
    * Returns a string of the response body or an empty string for any error
    */
+#ifndef SOUNDWOOFER_NO_API
   virtual std::string httpGet(const std::string endpoint) {
 #ifndef SOUNDWOOFER_CUSTOM_HTTP
     httplib::Client cli(BACKEND_URL, BACKEND_PORT);
@@ -606,6 +787,7 @@ private:
 #endif
     return NOT_IMPLEMENTED;
   }
+#endif
 
   /**
    * Will load the wav file to a float buffer in the SWImpulse struct
@@ -619,8 +801,8 @@ private:
       }
     }
     else {
-      const std::string cachedPath = mIrCacheDirectory + ir.id;
-      if (!drwav_init_file(&wav, cachedPath.c_str(), nullptr)) {
+      const std::string path = ir.local ? (mIrDirectory + ir.file) : (mIrCacheDirectory + ir.id);
+      if (!drwav_init_file(&wav, path.c_str(), nullptr)) {
         return NOT_CACHED;
       }
     }
@@ -669,27 +851,9 @@ private:
   }
 
   /**
-   * Creates a directory
-   * Returns success if the folder was created or already existing
+   * Writes a binary file, won't append to an existing one
    */
-  static Status createFolder(const char* path) {
-#ifdef _WIN32
-    const bool ok = CreateDirectory(path, nullptr);
-    if (!ok) {
-      if (GetLastError() != ERROR_ALREADY_EXISTS) {
-        return GENERIC_ERROR; // Probably permission, locked file etc
-      }
-    }
-    return SUCCESS;
-#else 
-    mode_t process_mask = umask(0);
-    int result_code = mkdir(path, S_IRWXU | S_IRWXG | S_IRWXO);
-    umask(process_mask);
-    return !result_code ? SUCCESS : GENERIC_ERROR;
-#endif
-  }
-
-  static Status writeFile(const char* path, const char* data, size_t length) {
+  static Status writeFile(const char* path, const char* data, const size_t length) {
     try {
       auto file = std::fstream(path, std::ios::out | std::ios::binary);
       file.write(data, length);
@@ -699,5 +863,15 @@ private:
     catch (...) {
       return GENERIC_ERROR;
     }
+  }
+
+  static bool isWaveName(std::string& name) {
+    return name.length() - name.find_last_of(".WAV") == 4
+        || name.length() - name.find_last_of(".wav") == 4;
+  }
+
+  static bool isJSONName(std::string& name) {
+    return name.length() - name.find_last_of(".JSON") == 5
+        || name.length() - name.find_last_of(".json") == 5;
   }
 };
